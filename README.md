6732b744dd893bf60e2e7bf0bd49634347f3ff4a
wayne <309334941@qq.com>
2024-09-11 13:46:52 -0500

    update the prepation scrip for dualsense c and rpi

--- README.md
+++ README.md
@@ -3,3 +3,15 @@ For dualsense controller and raspberry pi
 
 ## Description
 Demos for Dualsense controller and Raspberry Pi 3B+
+
+## Sep-11 2024
+- Using pydualsense to test communication with the controller and perform basic actions
+- Added pi-prep.sh to check if all prerequisites are met
+
+
+## Prerequisites
+- Raspberry Pi 3B+
+- Dualsense controller
+- Raspberry Pi OS Lite (64-bit)
+
+- [pydualsense](https://github.com/flok/pydualsense)
--- pi-prep.sh
+++ pi-prep.sh
@@ -19,6 +19,52 @@ check_internet() {
     fi
 }
 
-# Execute both functions
+check_python() {
+    echo "Checking Python version..."
+    python3 --version
+}
+
+check_kernel() {
+    echo "Checking kernel version..."
+    uname -r
+}
+
+# add rule to let user access the PS5 controller without root
+add_rule() {
+    echo "Adding rule to let user access the PS5 controller without root..."
+    sudo cp 70-ps5-controller.rules /etc/udev/rules.d
+    sudo udevadm control --reload-rules
+    sudo udevadm trigger
+}
+
+check_install_libhidapi() {
+    echo "Checking if libhidapi is installed..."
+    dpkg -l | grep libhidapi
+    # if not installed, install it
+    if [ $? -ne 0 ]; then
+        echo "libhidapi is not installed. Installing..."
+        sudo apt-get install libhidapi-dev
+    fi
+}
+
+
+check_install_pydualsense() {
+    echo "Checking if pydualsense is installed..."
+    pip list | grep pydualsense
+    # if not installed, install it
+    if [ $? -ne 0 ]; then
+        echo "pydualsense is not installed. Installing..."
+        pip install --upgrade pydualsense
+    fi
+}
+
+
+
+# Execute all
 check_usb
 check_internet
+check_python
+check_kernel
+check_install_libhidapi
+add_rule
+check_install_pydualsense
